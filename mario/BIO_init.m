612de2b3c5ecb858c2971a49c4cf61a5d84de1ff
raulest50 <raulest50@gmail.com>
2018-11-29 19:10:06 -0500

    se agrego un modelo de simulink con el sistema electrico simplificado y con el state space model listos para empezar el analisis de modelamiento de fallas y su deteccion y aislamiento

--- mario/BIO_init.m
+++
@@ -1,7 +0,0 @@
-function [ output_args ] = BIO_init( input_args )
-ttInitKernel('prioFP') 
-data.exectime = 1e-6; % se inicializa porque true time lo requiere pero no se usa 
-starttime = 4; % tiempo de arranque del controlador 
-period = 0.0001; % periodo de muestreo 
-ttCreatePeriodicTask('ctrl_task', starttime, period, 'BIO_c', data)
-end
