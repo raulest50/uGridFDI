396caba5d0f6b14e86653391bc26ec3e41ae6018
raulest50 <raulest50@gmail.com>
2018-12-12 15:06:02 -0500

    se volvieron a agregar unos archivos que se eliminaron por error para que funcione el modelo simplificado de la micro red

---
+++ SimulinkInit.m
@@ -0,0 +1,14 @@
+% este script es ejecutado al momento de ejecutar el modelo de simulink de
+% la microgrid de esta carpeta.
+% En la seccion de configuracion de simulink, En la pestaña de callBacks se
+% configuran los scripts que se desean ejectar al incio, durante o al
+% final de la simulacion.
+
+% se cargan las matrices dinamicas del MPC DMC.
+load('DMC_matrices');
+
+% se carga la libreria de true time.
+run('truetime-2.0\init_truetime');
+
+load('modeloElectrico');
+
