56d270fb88d0bfcf9b3471062119fd76c05e449f
raulest50 <raulest50@gmail.com>
2018-12-15 10:20:54 -0500

    se configuro la fuente de energia diesel con condiciones iniciales cercanas a 1000V que es el punto cercano de operacion cuando ya se estabiliza. gracias a esto ya no es necesario correr 5 segundos de la simulacion para lograr una tension estable en la entrada del inversor que usa el diesel. ahora la simulacion se ejecuta por 0.5 segundos.

--- uGrid_Hdetail/CodeGenTTime.m
+++ uGrid_Hdetail/CodeGenTTime.m
@@ -204,7 +204,7 @@ Pq_calculation = ...      % va  vb   ia  ib
 %%%%%%%%%%
 %
 BIO_initFcn = strrep(F_init_base, token_fname, Bio_ifname);
-BIO_initFcn = strrep(BIO_initFcn, token_st_time, num2str(5));
+BIO_initFcn = strrep(BIO_initFcn, token_st_time, num2str(.3));
 BIO_initFcn = strrep(BIO_initFcn, token_Ts, num2str(Ts));
 BIO_initFcn = strrep(BIO_initFcn, token_ftarget, Bio_fname);
 file_bio_init = fopen([Bio_ifname '.m'], 'w+');
@@ -247,7 +247,7 @@ fclose(file_BIO_c);
 %%%%%%%%%%
 %
 DIESEL_initFcn = strrep(F_init_base, token_fname, Diesel_ifname);
-DIESEL_initFcn = strrep(DIESEL_initFcn, token_st_time, num2str(5));
+DIESEL_initFcn = strrep(DIESEL_initFcn, token_st_time, num2str(.3));
 DIESEL_initFcn = strrep(DIESEL_initFcn, token_Ts, num2str(Ts));
 DIESEL_initFcn = strrep(DIESEL_initFcn, token_ftarget, Diesel_fname);
 file_diesel_init = fopen([Diesel_ifname '.m'], 'w+');
--- uGrid_Hdetail/DIESEL_init.m
+++ uGrid_Hdetail/DIESEL_init.m
@@ -1,7 +1,7 @@
 function [ output_args ] = DIESEL_init( input_args )
 ttInitKernel('prioFP') 
 data.exectime = 1e-6; % se inicializa porque true time lo requiere pero no se usa 
-starttime = 5; % tiempo de arranque del controlador 
+starttime = 0.3; % tiempo de arranque del controlador 
 period = 0.0001; % periodo de muestreo 
 ttCreatePeriodicTask('ctrl_task', starttime, period, 'DIESEL_c', data)
 end
